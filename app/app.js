c71dfd1f354fe09e7363b01300459dcdd16380a1
Samantha Cohen <samanthacohen294@gmail.com>
2015-03-28 17:30:42 +0000

    Added a theme and some layout changes

--- app/app.js
+++ app/app.js
@@ -3,8 +3,21 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngMaterial',
   'myApp.homeView'
 ]).
-config(['$routeProvider', function($routeProvider) {
+config(function($routeProvider, $mdThemingProvider) {
   $routeProvider.otherwise({redirectTo: '/homeView'});
-}]);
+  $mdThemingProvider.theme('default')
+    .primaryPalette('deep-orange', {
+      'default': '400', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '50', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('purple', {
+      'default': '200' // use shade 200 for default, and keep all other shades the same
+    });
+});
