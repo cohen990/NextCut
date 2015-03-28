9271e4c0becd40821c182640b322db6ffe0aaede
Dan Cohen <dan.cohen@mopowered.co.uk>
2015-03-28 15:13:00 +0000

    starting with material design

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.homeView', ['ngRoute'])
+angular.module('myApp.homeView', ['ngRoute', 'ngMaterial'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/homeView', {
