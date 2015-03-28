10751ff0aaa0a4c0d78b75a3767949d4a298578d
Samantha Cohen <samanthacohen294@gmail.com>
2015-03-28 14:56:08 +0000

    getting search nearby results and displaying

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -10,24 +10,25 @@ angular.module('myApp.homeView', ['ngRoute'])
 }])
 
 .controller('HomeViewController', ['$scope', function($scope)  {
-    var x = document.getElementById("finding");
+    var finding = document.getElementById("finding");
+
+    $scope.found = false;
+
+    $scope.searchResults = [];
+
+    $scope.limit = 3;
 
     $scope.getLocation = function() {
         console.log("hi");
         if (navigator.geolocation) {
-            var id = navigator.geolocation.watchPosition($scope.showPosition, $scope.showError);
+            var id = navigator.geolocation.watchPosition($scope.findHairdressers, $scope.showError);
         } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
+            finding.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
 
-    $scope.showPosition = function(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-        $scope.findHairdressers(position);
-    }
     $scope.showError = function(positionError){
-        x.innerHtml = "Sorry, something went wrong. Please try again later.";
+        finding.innerHtml = "Sorry, something went wrong. Please try again later.";
     }
 
     $scope.findHairdressers = function(position) {
@@ -41,29 +42,23 @@ angular.module('myApp.homeView', ['ngRoute'])
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             },
-            radius: 5000
+            radius: 5000,
+
         };
 
         service.nearbySearch(request, $scope.printResults)
     }
 
     $scope.printResults = function(results){
+        $scope.searchResults = results;
+        $scope.found = true;
+        $scope.$apply();
+
         console.log(results);
+    }
 
-        var finding = document.getElementById("finding");
-
-        var innerHtml = "<ul>\n";
-
-        for(var i = 0; i < 3; i++){
-            innerHtml +=
-            "\t<li>\n" +
-            results[i].name + " - " + results[i].vicinity +
-            "\n</li>\n";
-        }
-
-        innerHtml += "</ul>\n"
-
-        finding.innerHTML = innerHtml;
+    $scope.show3More = function(){
+        $scope.limit += 3;
     }
 
     $scope.getLocation();
