b759257a68b77178ef0433a1ffa848ec6f52971e
Samantha Cohen <samanthacohen294@gmail.com>
2015-03-28 13:02:14 +0000

    removing unneccessary boilerplate and starting with chopsnipjump

---
+++ app/homeView/homeView.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.homeView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/homeView', {
+    templateUrl: 'homeView/homeView.html',
+    controller: 'HomeViewController'
+  });
+}])
+
+.controller('HomeViewController', [function() {
+
+}]);
