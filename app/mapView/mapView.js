555624423aa4eea254c3f51618a2c00487dc27e8
Dan Cohen <dan.cohen@mopowered.co.uk>
2015-03-30 21:52:35 +0100

    directions in app

--- app/mapView/mapView.js
+++ app/mapView/mapView.js
@@ -10,13 +10,38 @@ angular.module('myApp.mapView', ['ngRoute', 'ngMaterial'])
 }])
 
 .controller('MapViewController', ['$scope', '$routeParams', function($scope, $routeParams)  {
-    $scope.initializeMap = function() {
-        var mapOptions = {
-            center: { lat: parseFloat($routeParams.latitude), lng: parseFloat($routeParams.longitude)},
-            zoom: 19
-        };
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
 
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                      mapOptions);
+    function intializeMap() {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      var chicago = new google.maps.LatLng( localStorage["originLatitude"] , localStorage["originLongitude"]);
+      var mapOptions = {
+        zoom:7,
+        center: chicago
+      }
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      directionsDisplay.setMap(map);
+    }
+
+    function calcRoute() {
+      var start = new google.maps.LatLng( localStorage["originLatitude"] , localStorage["originLongitude"]);
+      var end = new google.maps.LatLng( localStorage["destinationLatitude"] , localStorage["destinationLongitude"]);
+      var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(result);
+        }
+      });
+    }
+
+    $scope.initialize = function(){
+        intializeMap();
+        calcRoute();
     }
 }]);
