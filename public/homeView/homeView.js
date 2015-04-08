f93a80f379d23564223c2beca9eab7f6b22151c2
Samantha Cohen <samanthacohen294@gmail.com>
2015-04-08 21:42:19 +0100

    postcode lookup

--- public/homeView/homeView.js
+++ public/homeView/homeView.js
@@ -11,6 +11,38 @@ angular.module('myApp.homeView', ['ngRoute', 'ngMaterial'])
 
 .controller('HomeViewController', ['$scope', '$location', function($scope, $location)  {
     $scope.GoToResults = function(){
-        $location.path("/results")
+        $scope.$apply(function(){
+            $location.path("/results")
+        });
     };
+
+    $scope.PostcodeLookup = function(){
+        if($scope.postcode){
+            $scope.findLatitudeLongitude();
+        }
+    }
+
+    $scope.postcode = 'n19 4ta';
+
+    $scope.findLatitudeLongitude = function(){
+        var geocoder = new google.maps.Geocoder();
+        var request = {
+            address: $scope.postcode
+        }
+        geocoder.geocode(request, $scope.StoreLatitudeLongitude)
+    }
+
+    $scope.StoreLatitudeLongitude = function(results, status){
+        console.log(status);
+        if(status === "ZERO_RESULTS"){
+            $scope.$apply(function(){
+                $scope.postcode = "No Results Found!"
+            })
+            return;
+        }
+
+        localStorage["originLatitude"] = results[0].geometry.location.k;
+        localStorage["originLongitude"] = results[0].geometry.location.D;
+        $scope.GoToResults();
+    }
 }]);
